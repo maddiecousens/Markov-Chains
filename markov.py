96a9782b1aea9e92ee6bdfc9c02b1c499e4eae06
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-04 17:15:19 -0700

    Adjust for ngram model and begin with capital

--- markov.py
+++ markov.py
@@ -49,6 +49,37 @@ def make_chains(text_string):
         
     return chains
 
+def make_chains_n(text_string, n):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+
+    A chain will be a key that consists of a tuple of n values
+    and the value would be a list of the word(s) that follow those n
+    words in the input text.
+
+    For example:
+
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
+
+    chains = {}
+
+    words = text_string.split()
+
+    for i in xrange(len(words) - (n - 1)):
+        n_gram_key = []
+        for j in xrange(n):
+            n_gram_key.append(words[i + j])
+
+        if i < len(words) - n:
+            next_word = words[i+n]
+        else:
+            next_word = None
+
+        chains.setdefault(tuple(n_gram_key), []).append(next_word)
+
+    return chains
+
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
@@ -72,18 +103,47 @@ def make_text(chains):
     # Join list, return string
     return " ".join(text)
 
+def make_text_n(chains, n):
+    """Takes dictionary of markov chains; returns random text."""
+
+    # Randomly choose key from dictionary which will be a tuple
+    
+    capital_keys = [key for key in chains.keys() if key[0].istitle()]
+    starting_key = choice(capital_keys)
+
+    text = []
+    for i in xrange(n):
+        text.append(starting_key[i])
+
+    while True:
+        key_check = []
+        for j in xrange(-n, 0):
+            key_check.append(text[j])
+        next_word = choice(chains[tuple(key_check)])
+        if next_word:
+            text.append(next_word)
+        else:
+            break
+
+    return " ".join(text)
+
+
+
+
 
 input_path = sys.argv[1]
+n = int(sys.argv[2])
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains_n(input_text, n)
+# print chains
 
-# Produce random text
-random_text = make_text(chains)
+# # Produce random text
+random_text = make_text_n(chains, n)
 
 
 print random_text
